c07ec6972bff57e19ec15cedb841535be79807a5
Ayra Baet <ayra.austine@gmail.com>
2025-09-09 13:43:58 +0800

    Implement the Slice and Splice Algorithm

--- fundamentals-review/frankensplice.js
+++ fundamentals-review/frankensplice.js
@@ -1,7 +1,8 @@
 function frankenSplice(arr1, arr2, index) {
     copyArr = arr2.slice();
-
+    copyArr.splice(index, 0, ...arr1);
+    
     return copyArr;
 }
 
-console.log(frankenSplice([1, 2, 3, 4], [], 0));
+console.log(frankenSplice([1, 2, 3], [4, 5], 1));
