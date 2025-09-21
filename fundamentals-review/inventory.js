bcdbcf01952f9373880e589b87625946d623143f
Ayra Baet <ayra.austine@gmail.com>
2025-09-21 10:10:52 +0800

    Added the removeProduct function

--- fundamentals-review/inventory.js
+++ fundamentals-review/inventory.js
@@ -1,4 +1,4 @@
-let inventory = [];
+const inventory = [];
 
 function findProductIndex(productName) {
     return inventory.findIndex(product => product.name.toLowerCase() === productName.toLowerCase());
@@ -17,12 +17,36 @@ function addProduct(product) {
     }
 }
 
-console.log(findProductIndex("FloUr"));
-inventory.push({name: "FLOUR", quantity: 5});
-console.log(findProductIndex("flour"));
+function removeProduct(productName, quantityToSubtract) {
+    const nameLowerCase = productName.toLowerCase();
+    const index = findProductIndex(productName);
 
-addProduct({name: "FLOUR", quantity: 5});
-console.log(inventory[0].quantity);
+        if (index === -1) {
+            console.log(`${nameLowerCase} not found`);
+            return;
+        }
 
+        if (inventory[index].quantity < quantityToSubtract) {
+            console.log(`Not enough ${nameLowerCase} available, remaining pieces: ${inventory[index].quantity}`);
+            return;
+        } else {
+            inventory[index].quantity -= quantityToSubtract;
+            console.log(`Remaining ${nameLowerCase} pieces: ${inventory[index].quantity}`);
+
+            if (inventory[index].quantity === 0) {
+                inventory.splice(index, 1);
+            }
+        }
+}
+
+addProduct({name: "flour", quantity: 5});
 addProduct({name: "rice", quantity: 5});
+addProduct({name: "flour", quantity: 3});
 console.log(inventory);
+
+console.log(findProductIndex("FLOUR"));
+console.log(findProductIndex("meat"));
+
+removeProduct("FLOur", 8);
+removeProduct("rice", 10);
+removeProduct("bread", 4);
