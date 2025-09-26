e344322801a345ce34fa73726ddce61c6ba772bb
Ayra Baet <ayra.austine@gmail.com>
2025-09-26 13:29:15 +0800

    Build a Password Generator App

---
+++ fundamentals-review/generatePass.js
@@ -0,0 +1,13 @@
+function generatePassword(length) {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+    let result = "";
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        result += chars[randomIndex];
+    }
+    return result;
+}
+
+const password = generatePassword(10);
+console.log(`Generated password: ${password}`);
