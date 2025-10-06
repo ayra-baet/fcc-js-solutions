54b62f0d9370c0c609c5040663aeb8e915a56fe9
Ayra Baet <ayra.austine@gmail.com>
2025-10-06 13:09:12 +0800

    Build an Odd Fibonacci Sum Calculator

--- fundamentals-review/sumFibs.js
+++ fundamentals-review/sumFibs.js
@@ -1,5 +1,19 @@
 function sumFibs(num) {
-    return 0;
+    let prevNum = 0;
+    let currNum = 1;
+    let oddSum = 0;
+
+    while (currNum <= num) {
+        if (currNum % 2 !== 0) {
+            oddSum += currNum;
+        }
+
+        [prevNum, currNum] = [currNum, prevNum + currNum];
+    }
+    
+    return oddSum;
 }
 
-console.log(sumFibs(10));
+console.log(sumFibs(1000));
+console.log(sumFibs(4000000));
+console.log(sumFibs(75024));
