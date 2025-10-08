1579167f10a41491f2fd9f08c66c47dbfac693c4
Ayra Baet <ayra.austine@gmail.com>
2025-10-08 13:13:55 +0800

    Added checks for the number of arguments passed

--- fundamentals-review/add-together.js
+++ fundamentals-review/add-together.js
@@ -1,11 +1,29 @@
-function addTogether(num1, num2) {
-    if (typeof num1 !== "number" || typeof num2 !== "number") {
-        return undefined;
+function addTogether(...num) {
+    if (num.length === 2) {
+        if (typeof num[0] !== "number" || typeof num[1] !== "number") {
+            return undefined;
+        } else {
+            return num[0] + num[1];
+
+        }
     }
 
-    return num1 + num2;
+    if (num.length === 1) {
+        if (typeof num[0] === "number") {
+            return function(secondNum) {
+                if (typeof secondNum === "number") {
+                    return num[0] + secondNum;
+                }
+            }
+        }
+    }    
 }
+
 
 console.log(addTogether(2, 3));
 console.log(addTogether("2", 3));
 console.log(addTogether(5, undefined));
+console.log(addTogether("https://www.youtube.com/watch?v=dQw4w9WgXcQ"));
+console.log(addTogether(5))
+console.log(addTogether(5)(7));
+console.log(addTogether(2)([3]));
