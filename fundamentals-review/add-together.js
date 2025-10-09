9b3f978534df6a403bc13d4a1feb798337b83a80
Ayra Baet <ayra.austine@gmail.com>
2025-10-09 16:18:34 +0800

    Build an Optional Arguments Sum Function

--- fundamentals-review/add-together.js
+++ fundamentals-review/add-together.js
@@ -1,24 +1,17 @@
 function addTogether(...num) {
-    if (num.length === 2) {
-        if (typeof num[0] !== "number" || typeof num[1] !== "number") {
-            return undefined;
-        } else {
-            return num[0] + num[1];
 
+    if (typeof num[0] !== "number") return undefined;
+
+    if (num.length === 1) {
+        return function(secondNum) {
+            return typeof secondNum === "number" ? num[0] + secondNum : undefined;
         }
     }
 
-    if (num.length === 1) {
-        if (typeof num[0] === "number") {
-            return function(secondNum) {
-                if (typeof secondNum === "number") {
-                    return num[0] + secondNum;
-                }
-            }
-        }
-    }    
-}
+    if (typeof num[1] !== "number") return undefined;
 
+    return num[0] + num[1];   
+}
 
 console.log(addTogether(2, 3));
 console.log(addTogether("2", 3));
