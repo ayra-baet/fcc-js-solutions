a7a0e8091963d2bfe202eb7315bcc913069b198b
Ayra Baet <ayra.austine@gmail.com>
2025-10-21 16:30:23 +0800

    Implement an Element Skipper

---
+++ fundamentals-review/drop-elements.js
@@ -0,0 +1,12 @@
+function dropElements(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            return arr.slice(i);
+        }
+    }
+    return [];
+}
+
+console.log(dropElements([1, 2, 3, 4], function(n) {return n >= 3;}));
+console.log(dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;}));
+console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}));
