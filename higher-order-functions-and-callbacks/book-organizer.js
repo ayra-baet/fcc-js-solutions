059768b5089a8bd22a25ae94d63beb3bd800caf6
Ayra Baet <ayra.austine@gmail.com>
2025-10-31 09:05:25 +0800

    Initial commit for book organizer

---
+++ higher-order-functions-and-callbacks/book-organizer.js
@@ -0,0 +1,19 @@
+const books = [
+    {
+        title: "Echoes of the Forgotten Sky",
+        authorName: "Celeste Marlowe",
+        releaseYear: 2026
+    },
+
+    {
+        title: "The Cartographer's Paradox",
+        authorName: "Elias Finch",
+        releaseYear: 2024
+    },
+
+    {
+        title: "Beneathe the Willow's Spell",
+        authorName: "Mira Thorne",
+        releaseYear: 1925
+    }
+];
