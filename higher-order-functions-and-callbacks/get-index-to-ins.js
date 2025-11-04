d147648842de88484e5acd3d244060321e9437ab
Ayra Baet <ayra.austine@gmail.com>
2025-11-04 10:25:48 +0800

    Implement a Sorted Index Finder

--- higher-order-functions-and-callbacks/get-index-to-ins.js
+++ higher-order-functions-and-callbacks/get-index-to-ins.js
@@ -1,5 +1,13 @@
 function getIndexToIns(arr, num) {
-    return arr.sort((a , b) => a - b);
+    arr.sort((a , b) => a - b);
+
+    const isLargerOrEqualToNum = element => element >= num;
+    const index = arr.findIndex(isLargerOrEqualToNum);
+
+    return index === -1 ? arr.length : index;
 }
 
+console.log(getIndexToIns([2, 20, 10], 19));
 console.log(getIndexToIns([5, 3, 20, 3], 5));
+console.log(getIndexToIns([3, 10, 5], 11));
+console.log( getIndexToIns([], 5));
