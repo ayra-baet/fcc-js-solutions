6404e19b03fe318d59acb9743244433b5e4ce654
Ayra Baet <ayra.austine@gmail.com>
2025-11-14 07:43:53 +0800

    Build a Prime Number Sum Calculator

--- higher-order-functions-and-callbacks/sumPrimes.js
+++ higher-order-functions-and-callbacks/sumPrimes.js
@@ -1,8 +1,26 @@
-function isPrime(num) {
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) return false;
-    }
-    return true;
+function sumPrimes(num) {
+const primes = [];
+
+    // immediately return 0 if input number is less than 2
+    if (num < 2) return 0;
+
+    // helper function to check if a number is prime
+   function isPrime(n) {
+        for (let i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i === 0) return false;
+        }
+
+        return true;
+   }
+
+   // push prime numbers to primes array
+   for (let i = 2; i <= num; i++) {
+        if (isPrime(i)) primes.push(i);
+   }
+
+   // computes for the sum of the prime numbers
+   return primes.reduce((sum, prime) => sum + prime);
 }
-console.log(isPrime(10));
-console.log(isPrime(977));
+
+console.log(sumPrimes(10));
+console.log(sumPrimes(977));
